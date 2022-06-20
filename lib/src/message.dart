cb8d425cce6dc383f8d2ceeca28bbbebd35911d5
mawais38 <mawais3665@gmail.com>
2022-06-20 15:54:31 +0500

    example added

--- lib/src/message.dart
+++ lib/src/message.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 class MessageWidget extends StatelessWidget {
   const MessageWidget({
+    /// declaring variable
     Key? key,
     required this.id,
     required this.name,
@@ -25,110 +26,123 @@ class MessageWidget extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.black,
         body: Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.only(top: 20.0),
-                child: CircleAvatar(
-                  backgroundImage: AssetImage(imageUrl),
-                  maxRadius: 30,
-                ),
+          ///setting cross axis to start to get inline
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+
+              /// sender image
+              child: CircleAvatar(
+                backgroundImage: AssetImage(imageUrl),
+                maxRadius: 30,
               ),
-              const SizedBox(
-                width: 16,
-              ),
-              Expanded(
-                child: Container(
-                  color: Colors.transparent,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 28.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: <Widget>[
-                        Row(
-                          children: [
-                            Text(
-                              name,
-                              style: const TextStyle(
-                                fontSize: 16,
-                                color: Colors.red,
-                              ),
+            ),
+            const SizedBox(
+              width: 16,
+            ),
+            Expanded(
+              child: Container(
+                color: Colors.transparent,
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 28.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Row(
+                        children: [
+                          ///sender name
+                          Text(
+                            name,
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.red,
                             ),
-                            const SizedBox(width: 20),
-                            Text(
-                              time,
-                              style: TextStyle(
-                                fontSize: 12,
-                                color: Colors.grey.shade600,
-                              ),
-                            ),
-                          ],
-                        ),
-                        const SizedBox(
-                          height: 6,
-                        ),
-                        Text(
-                          messages,
-                          style: const TextStyle(
-                            fontSize: 13,
-                            color: Colors.white,
                           ),
+                          const SizedBox(width: 20),
+
+                          ///time
+                          Text(
+                            time,
+                            style: TextStyle(
+                              fontSize: 12,
+                              color: Colors.grey.shade600,
+                            ),
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 6,
+                      ),
+
+                      ///message body
+                      Text(
+                        messages,
+                        style: const TextStyle(
+                          fontSize: 13,
+                          color: Colors.white,
                         ),
-                        const SizedBox(
-                          height: 5,
-                        ),
-                        Row(
-                          children: [
-                            Container(
-                              height: 20,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.circular(3),
-                                  color: Colors.grey),
-                              child: Center(
-                                child: Text(
-                                  repNo,
-                                  style: const TextStyle(
-                                    fontSize: 10,
-                                    color: Colors.white,
-                                  ),
+                      ),
+                      const SizedBox(
+                        height: 5,
+                      ),
+                      Row(
+                        children: [
+                          Container(
+                            height: 20,
+                            width: 50,
+                            decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(3),
+                                color: Colors.grey),
+                            child: Center(
+                              /// reply on that message
+                              child: Text(
+                                repNo,
+                                style: const TextStyle(
+                                  fontSize: 10,
+                                  color: Colors.white,
                                 ),
                               ),
                             ),
-                            const SizedBox(width: 15),
-                            const Icon(Icons.emoji_emotions_sharp,
-                                color: Color(0xfff4c13f)),
-                            Text(
-                              no.toString(),
-                              style: const TextStyle(
-                                fontSize: 13,
-                                color: Colors.white,
-                              ),
+                          ),
+                          const SizedBox(width: 15),
+                          const Icon(Icons.emoji_emotions_sharp,
+                              color: Color(0xfff4c13f)),
+                          Text(
+                            no.toString(),
+                            style: const TextStyle(
+                              fontSize: 13,
+                              color: Colors.white,
                             ),
-                            const SizedBox(
-                              width: 22,
-                            ),
-                            const Icon(Icons.emoji_emotions_rounded,
-                                color: Color(0xfff4c13f)),
-                            Text(
-                              no.toString(),
-                              style: const TextStyle(color: Colors.white),
-                            ),
-                            const SizedBox(
-                              width: 15,
-                            ),
-                             const Icon(
-                              Icons.face_retouching_natural_rounded,size: 28.0,color: Colors.grey,
-                             )
-                          ],
-                        ),
-                      ],
-                    ),
+                          ),
+                          const SizedBox(
+                            width: 22,
+                          ),
+                          const Icon(Icons.emoji_emotions_rounded,
+                              color: Color(0xfff4c13f)),
+
+                          ///reply count
+                          Text(
+                            no.toString(),
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                          const SizedBox(
+                            width: 15,
+                          ),
+                          const Icon(
+                            Icons.face_retouching_natural_rounded,
+                            size: 28.0,
+                            color: Colors.grey,
+                          )
+                        ],
+                      ),
+                    ],
                   ),
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
+        ),
       ),
     );
   }
